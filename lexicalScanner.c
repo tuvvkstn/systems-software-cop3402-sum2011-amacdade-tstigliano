1fe5d4cf1c0d16b8cb8afc995774bcdabfa7694d
tystigliano@gmail.com <tystigliano@gmail.com@ee440e03-e195-b5f4-3287-89ddcc88ae29>
2011-06-17 05:50:22 +0000

    My thoughts behind how this should work are the program should read in a line at a time from the file, trace through each line, separate the characters into their respective tokens, and store these tokens into an array of struct tokens. On the note of the struct tokens created in the lexicalScanner.h file, I pulled it directly from the power points so I'm not sure what other information, if any, will be needed for each token.

--- lexicalScanner.c
+++ lexicalScanner.c
@@ -5,3 +5,62 @@
  *	Professor: Dr. Euripides Montagne
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "lexicalScanner.h"
+
+int main(){
+	
+	char filename[CMAX];
+	printf("PL/O Lexical analyser");
+	printf("Enter filename: ");
+
+	scanf("%s", filename);
+	ifp = fopen(filename, "r");
+	ofp = fopen("output.txt", "w");
+
+	if(!ifp){
+		printf("Invalid file name. Program exiting");
+		return;
+	}
+
+	initialize();
+	
+	while(currChar != EOF){
+		// reads through the file until it gets to the end
+	}
+
+
+}
+
+
+// Initializes the error array and initializes the counters
+void initalize(){
+	int i;
+
+	for( i = 0; i < ERRMAX; i ++){
+		errors[i] = FALSE;
+	}
+ 
+	charCount = 0; 
+	lineCount = 0;
+	line[0] = '\0';
+}
+
+
+// The idea I had for this function was to read in an line at a time and
+// then trace through that line tokenizing the appropritate words
+void nextCharacter(){
+	
+	
+}
+
+
+
+// This function will read through white space and comments
+// when encountered
+void skip(){
+	
+}
