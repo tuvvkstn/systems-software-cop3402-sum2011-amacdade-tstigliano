86a06a778644c052b38963c913a60658d212cc2a
tystigliano <tystigliano@gmail.com>
2011-06-20 07:13:15 +0000

    My revised version

--- lexicalanalyzer.h
+++ lexicalanalyzer.h
@@ -1,62 +1,82 @@
 /*
- * lexicalanalyzer.h
- *
- *  Created on: Jun 19, 2011
- *      Author: Austin
+ *      Authors: Tyler Stigliano, Austin Macdade
+ *      Implementation of a Lexical Scanner for the language described in class
+ *      Class: COP3402: System Software
+ *      Professor: Dr. Euripides Montagne
  */
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #ifndef LEXICALANALYZER_H_
 #define LEXICALANALYZER_H_
 
-enum LEXICAL_ERRORS {
-	NO_ERROR,
-	ERROR_INVALID_SYMBOL,
-	ERROR_INVALID_IDENTIFIER,
-	ERROR_INVALID_FILE,
-	ERROR_INVALID_STATE,
-	ERROR_ARRAY_INDEX_OUT_OF_BOUNDS
+// Constants
+#define CMAX 12         // Maximum number of character length
+#define DMAX 5          // Maximum number of digit length
+#define LMAX 101        // Maximum line length
+#define ERRMAX 4        // Maximum error number
+#define TRUE 1
+#define FALSE 0
+
+// Token types
+typedef enum    {nulsym = 1, identsym, numbersym, plussym, minussym, 
+                 multsym, slashsym, oddsym, eqlsym, neqsym, lessym, leqsym, 
+                 gtrsym, geqsym, lparentsym, rparentsym, commasym, 
+                 semicolonsym, periodsym, becomessym, 
+				 } token_type;
+
+typedef enum { beginsym = 21, endsym, ifsym, thensym, whilesym, dosym, 
+			   callsym, constsym, intsym, procsym, outsym, insym, 
+			   elsesym} special_token;
+
+
+
+struct token{
+        int kind;                               // token type
+        char name[CMAX+1];						// name 
+		int value;								//
+		int hasValue;
 };
 
-enum LEXICAL_SYMBOLS {	// symbol table is a modified version of the one in the assignment so that some of the values can be entered as a mathematically derived value of their ascii value
-	NULL_SYM,
-	ID_SYM,
-	NUMBER_SYM,
-	ODD_SYM,
-	BECOMES_SYM,
-	BEGIN_SYM,
-	END_SYM,
-	IF_SYM,
-	THEN_SYM,
-	WHILE_SYM,
-	DO_SYM,
-	CALL_SYM,
-	CONST_SYM,
-	INT_SYM,
-	PROCEDURE_SYM,
-	OUT_SYM,
-	IN_SYM,
-	ELSE_SYM,
-	LEFT_PARENTHESIS_SYM,
-	RIGHT_PARENTHESIS_SYM,
-	MULTIPLY_SYM,
-	PLUS_SYM,
-	COMMA_SYM,
-	MINUS_SYM,
-	PERIOD_SYM,
-	FORWARD_SLASH_SYM,
-	SEMICOLON_SYM,
-	LESS_THAN_SYM,
-	EQUAL_SYM,
-	GREATER_THAN_SYM,
-	NOT_EQUAL_SYM,
-	LESS_THAN_OR_EQUAL_SYM,
-	GREATER_TH_OR_EQUAL_SYM
-};
+// Reserved words
 
-int setInputFile(char* filename);
-int setOutputFile(char* filename, char* mode); // mode included so that users can choose between write and append
-int generateTokenList(int* list);
+
+char* reservedWords [] ={ "begin", "end", "if", "then", "while", "do", "call", "const",
+						  "int", "proc", "out", "in", "else"};
+
+int numReservWords = sizeof(reservedWords) / sizeof(char *);               // number of keywords
+
+// Name of Special Symboles
+char * sSymbolesName[] = {"plus", "minus", "times", "slash", "comma", "rparetn",
+                          "lparent", "rbracket", "lbracket", "equal", "comman", "period",
+                           "lss", "grt", "leq", "geq", "semicolon", "colon"};
+
+int numSpecials = sizeof(sSymbolesName)/ sizeof(char *);                // number of special characters
+
+//int numTotalSymboles = numSpecials + numKeyWords;                               // number of special characters and keywords
+
+// Global Variables
+char currChar;                  // Current character
+char line[LMAX];                // Current line
+int charCount;                  // Character counter
+int lineCount;                  // line counter
+int errors[ERRMAX];
+struct token nameRec[500];
+int h;
+
+FILE* ifp, *ofp; 
+
+
+// Functions
+void errorFound(int type);              // Mark an error
+void initialize();                              // Initialize variables
+void nextCharacter();                   // Description in source file. 
+void skip();                                    // Skips whitespace and comments
+void getToken();								// Gets the token
+void printLexemeTable();
+
 
 #endif /* LEXICALANALYZER_H_ */
